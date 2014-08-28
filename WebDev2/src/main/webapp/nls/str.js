f045d8a136758ed24e72702b25e91f6d5a4908df
leekyunghee <qkqhlkh@gmail.com>
2014-08-28 16:13:44 +0900

    다국어 지원 테스트

---
+++ WebDev2/src/main/webapp/nls/str.js
@@ -0,0 +1,15 @@
+define({ 
+"root" : { 
+	"id" : "아이디", 
+	"password" : "비밀번호", 
+	"sign_in" : "로그인", 
+	"close" : "닫기", 
+	"cancel" : "취소", 
+	"ok" : "확인", 
+	"requireUsername" : "아이디를 넣어주십시오.", 
+	"requirePassword" : "비밀번호를 넣어주십시오.", 
+	"requireRole" : "역할을 선택해 주십시오.",
+	'invalidLogin' : "아이디 혹은 비밀번호가 잘못 입력되었습니다. "
+}, 
+ 	"en-us" : true 
+}); 

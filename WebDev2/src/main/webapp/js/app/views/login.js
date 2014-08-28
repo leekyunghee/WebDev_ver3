7e4b7543b76726d1b25a37723dcdfec45f3be228
leekyunghee <qkqhlkh@gmail.com>
2014-08-28 16:01:52 +0900

    로그인 성공이나 실패할 경우 메세지를 팝업 한다.

--- WebDev2/src/main/webapp/js/app/views/login.js
+++ WebDev2/src/main/webapp/js/app/views/login.js
@@ -2,36 +2,74 @@ define(function(require) {
 
 	"use strict";
 
-	var $ 					= require('jquery');
-	var _ 					= require('underscore');
-	var Backbone 			= require('backbone');
+	var $ = require('jquery');
+	var _ = require('underscore');
+	var Backbone = require('backbone');
 
-	var model				= require('models/login');
-	var tpl 				= require('text!tpl/login.html');
+	var LoginModel = require('models/login');
+	var loginModel = new LoginModel();
+	var tpl = require('text!tpl/login.html');
 	var template = _.template(tpl);
 
+	// require i18n
+	var locale = require('i18n!nls/str');
+
 	return Backbone.View.extend({
+
+		model : loginModel,
+		initalize : function(options) {
+			console.log("login view initialize()");
+		},
+
 		render : function() {
 			this.$el.html(template());
+			$('#userid').prop('placeholder', locale.id);
+			$('#password').prop('placeholder', locale.password);
+			$('#loginBtn').html(locale.sign_in);
 			return this;
 		},
 		events : {
 			"click #loginBtn" : "login"
 		},
-		login : function() {
+		login : function(event) {
 			var userid = $('#userid').val();
 			var password = $('#password').val();
-		
-			model.set({id:userid, pwd:password});
-			model.login({
-				url: 'login/index'
+
+			loginModel.set({
+				id : userid,
+				pwd : password
+			}, {
+				silent : true
 			});
-			
-			if (model.get('successYn') == true){
-    			location.hash = '#employeeList';
-    		}else {
-    			alert("id or password is not valid");
-    		}
+			if (loginModel.isValid()) {
+				loginModel.login({
+					success : this.success
+				});
+			} else {
+				Backbone.AlertView.msg($('#alert'), {
+					alert : "warning",
+					message : loginModel.validationError
+				});
+			}
+		},
+
+		success : function() {
+			console.log(loginModel.toJSON());
+			if (loginModel.get('successYn')) {
+				Backbone.pageMove.dashboard();
+			} else {
+				Backbone.AlertView.msg($('#alert'), {
+					alert : 'warning',
+					message : locale.invalidLogin
+				});
+				Backbone.ModalView.msg({
+					title : 'warning',
+					body : locale.invalidLogin
+				});
+			}
+		},
+		error : function() {
+			console.log("error");
 		}
 	});
 });
